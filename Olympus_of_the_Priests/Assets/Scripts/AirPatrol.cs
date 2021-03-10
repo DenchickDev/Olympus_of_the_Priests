fb63f32753ab9c0a21fe11ff8ec18aa638ea7789
Иванов Денис <ltybc3528@mail.ru>
2021-03-10 23:28:27 +0300

    Task3: добавил патрулирование для врагов по осям x и y

---
+++ Olympus_of_the_Priests/Assets/Scripts/AirPatrol.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirPatrol : MonoBehaviour
+{
+    /// <summary>
+    /// Первая точка патруля
+    /// </summary>
+    public Transform point1;
+
+    /// <summary>
+    /// Вторая точка патруля
+    /// </summary>
+    public Transform point2;
+
+    /// <summary>
+    /// Скорость, перемещения между точками
+    /// </summary>
+    public float speed = 2f;
+
+    /// <summary>
+    /// Время ожидания возле точек
+    /// </summary>
+    public float waintTime = 2f;
+
+    /// <summary>
+    /// Признак: должен ли двигаться
+    /// </summary>
+    private bool isMove = true;
+    
+    /// <summary>
+    /// Тип патрулирования
+    /// </summary>
+    public enum TypePatrol
+    {
+        /// <summary>
+        /// Патруль по вертикали
+        /// </summary>
+        VerticalPatrol,
+
+        /// <summary>
+        /// Патруль по горизонтали
+        /// </summary>
+        HorizontalPatrol,
+    }
+
+    /// <summary>
+    /// Текущее тип патрулирования
+    /// </summary>
+    public TypePatrol typePatrol = TypePatrol.HorizontalPatrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(point1.transform.position.x, point1.transform.position.y, point1.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point1.position, speed * Time.deltaTime);
+        }
+
+        if (transform.position == point1.position)
+        {
+            Transform swapVal = point1;
+            point1 = point2;
+            point2 = swapVal;
+            isMove = false;
+            StartCoroutine(Wainting());
+            
+        }
+    }
+    
+    IEnumerator Wainting()
+    {
+        yield return new WaitForSeconds(waintTime);
+        isMove = true;
+        if(typePatrol == TypePatrol.HorizontalPatrol)
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
+    }
+}
---
+++ Olympus_of_the_Priests/Assets/Scripts/GroundPatrol.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPatrol : MonoBehaviour
+{
+    /// <summary>
+    /// Скорость движения
+    /// </summary>
+    public float speed = 1f;
+
+    /// <summary>
+    /// Радиус зоны патрулирования
+    /// </summary>
+    public float RadiusOfPatrol = 5f;
+
+    /// <summary>
+    /// Центр зоны патрулирования
+    /// </summary>
+    public Transform point;
+
+    /// <summary>
+    /// Движется ли вправо
+    /// </summary>
+    private bool _moveingRight;
+
+    /// <summary>
+    /// Движется ли вправо
+    /// </summary>
+    private bool moveingRight
+    {
+        get { return _moveingRight; }
+        set
+        {
+            if (value != _moveingRight)
+            {
+                isMove = false;
+                StartCoroutine(Wainting());
+            }
+            _moveingRight = value;
+        }
+    }
+
+    /// <summary>
+    /// Время ожидания возле
+    /// границы зоны патрулирования
+    /// </summary>
+    public float waintTime = 2f;
+
+    /// <summary>
+    /// Признак: должен ли двигаться
+    /// </summary>
+    private bool isMove = true;
+
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDirection();
+        Chill();
+        print(rb.velocity.x);
+    }
+
+    /// <summary>
+    /// Метод патрулирования, вправо/влево
+    /// </summary>
+    void Chill()
+    {
+        if (isMove)
+        {
+            //Задаётся позиция патрулирования + точка NPC = идти налево.
+            if (moveingRight)
+            {
+                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+            }
+            //Задаётся позиция патрулирования - точка NPC = идти направо.
+            else
+            {
+                transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Определяет направление движения
+    /// </summary>
+    private void CheckDirection()
+    {
+        // Проверка движения.
+        if (transform.position.x > point.position.x + RadiusOfPatrol)
+        {
+            moveingRight = false;
+        }
+        else if (transform.position.x < point.position.x - RadiusOfPatrol)
+        {
+            moveingRight = true;
+        }
+    }
+
+    IEnumerator Wainting()
+    {
+        yield return new WaitForSeconds(waintTime);
+        isMove = true;
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+
+    }
+
+    /// <summary>
+    /// Отрисовывает радиус атаки на сцене,
+    /// при выделении объекта
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        if (point != null)
+        {
+            Vector3 pointPatrol1 = new Vector3(point.position.x - RadiusOfPatrol, point.position.y, point.position.z);
+            Vector3 pointPatrol2 = new Vector3(point.position.x + RadiusOfPatrol, point.position.y, point.position.z);
+            Gizmos.DrawLine(pointPatrol1, pointPatrol2);
+        }
+        //Gizmos.DrawWireSphere(point.position, attackRange);
+    }
+
+}
