49c7c2a78b78bfd13ea167d5219e8205ca43bff5
Иванов Денис <ltybc3528@mail.ru>
2021-03-16 22:05:24 +0300

    Рефакторинг: объединил Ground_ы в один объект, сделал скрипт FallPlatform.cs более гибким

--- Olympus_of_the_Priests/Assets/Scripts/AirPatrol.cs
+++ Olympus_of_the_Priests/Assets/Scripts/AirPatrol.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//тест
+
 public class AirPatrol : MonoBehaviour
 {
     /// <summary>
--- Olympus_of_the_Priests/Assets/Scripts/FallPlatform.cs
+++ Olympus_of_the_Priests/Assets/Scripts/FallPlatform.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class FallPlatform : MonoBehaviour
 {
-    
+    /// <summary>
+    /// Время после прикосновения и до падения
+    /// </summary>
+    [SerializeField]
+    private float timeToFall = 0.5f;
+
+    /// <summary>
+    /// Время после прикосновения и до уничтожения
+    /// </summary>
+    [SerializeField]
+    private float timeToDestroy = 2f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
       if (collision.gameObject.tag == "Player")
       {
-            Invoke("FallingPlatform", 0.5f);
-            Destroy(gameObject, 2f); 
+            Invoke("FallingPlatform", timeToFall);
+            Destroy(gameObject, timeToDestroy); 
       }
       
     }
+
     private void FallingPlatform ()
     {
         gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
