2ea9dc06d5fd960978ce75980a88c3775d615263
Данил Пахомов <Wanty3@mail.ru>
2021-03-20 00:11:06 +0300

    Task 14: Добавлена выдвижная ловушка в горизонтальной и вертикальной плоскостях

---
+++ Olympus_of_the_Priests/Assets/Scripts/PulloutY_axisTrap.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulloutY_axisTrap : MonoBehaviour
+{
+    public float speed = 4f;
+    bool isWait = false;
+    bool isHidden = false;
+    public float waitTime = 4f;
+    public Transform point;
+    [SerializeField]
+    public bool Down = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Down == false)
+        {
+            point.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+        }
+        else
+        {
+            point.transform.position = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Down == false)
+        {
+
+            if (isWait == false)
+                transform.position = Vector3.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
+            if (transform.position == point.position)
+            {
+                if (isHidden == true)
+                {
+                    point.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+                    isHidden = false;
+                }
+                else
+                {
+                    point.transform.position = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+                    isHidden = true;
+                }
+                isWait = true;
+                StartCoroutine(Waiting());
+            }
+        }
+        else
+        {
+            if (isWait == false)
+                transform.position = Vector3.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
+            if (transform.position == point.position)
+            {
+                if (isHidden)
+                {
+                    point.transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+                    isHidden = false;
+                }
+                else
+                {
+                    point.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+                    isHidden = true;
+                }
+                isWait = true;
+                StartCoroutine(Waiting());
+            }
+
+        }
+    }
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        isWait = false;
+    }
+
+}
+
